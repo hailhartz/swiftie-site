13f36a693412e43bc0d10f55a5f563d826529b72
Hailey <hhartz@umich.edu>
2023-11-23 23:43:35 -0500

    finished fly out and fly in effect for reputation and speak now songs

--- js/eraChoice.js
+++ js/eraChoice.js
@@ -1,199 +1,33 @@
-addEventListener("load", function() {
-
-    // function folklore_songs() {
-    //     return <>
-    //          <div class="song">
-    //             <h3>august</h3>
-    //             <p>
-    //                 Folklore is unique from Swift's other albums since 
-    //                 in this album, instead of writing songs solely based on 
-    //                 her own experiences, she expanded to telling 
-    //                 completely fictional stories. "august" tells the bittersweet story of 
-    //                 a love triangle from the "other" woman's perspective.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6nK2pIKFcRc5frrZKHgsiT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    //         </div>
-    //         <div class="song">
-    //             <h3>this is me trying</h3>
-    //             <p>
-    //                 The song "this is me trying" discusses a lot of 
-    //                 the struggles that come with important transitions in life. 
-    //                 The subject of the song grapples with moving on from 
-    //                 a breakup and generally echoes a feeling of being lost in life.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7cm50Lw03k6VvRauJtkyTj?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    //         </div>
-    //         <div class="song">
-    //             <h3>my tears ricochet</h3>
-    //             <p>
-    //                 I've always thought that "my tears ricochet" was 
-    //                 written from the perspective of a ghost looking over 
-    //                 her funeral. I like how the song a very haunting feeling. 
-    //                 The song discusses the subject's greivances with 
-    //                 a former partner or friend.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5P2bHCDM2tsgIaYWsZMhu5?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    //         </div>
-    //     </>
-    // };
-
-    // function speakNowSongs() {
-    //     return <>
-    //     <div class="song">
-    //     <h3>Ours</h3>
-    //     <p>"Ours" is a song that discusses a happy relationship between two people
-    //         that continues in spite of the dreary world surrounding them. Swift
-    //         wrote this song about her relationship with John Mayer, who 
-    //         was 10 years older than her at the time, but I think that the song
-    //         is sweet nonetheless.
-    //     </p>
-    //     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3yNJkriPzWjkkDAWHIAVUq?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    // </div>
-
-    // <div class="song">
-    //     <h3>Enchanted</h3>
-    //     <p>
-    //         "Enchanted" chronicles the experience of meeting someone for the first
-    //         time and being "enchanted" by them. Allegedly the song is about Adam Young, 
-    //         the lead singer of Owl City. He wrote a response song but it was released 
-    //         after the moment of "Enchanted" passed.
-    //     </p>
-    //     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3sW3oSbzsfecv9XoUdGs7h?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    // </div>
-
-    // <div class="song">
-    //     <h3>Speak Now</h3>
-    //     <p>
-    //         The thesis of Swift's album is embodied by "Speak Now," which tells 
-    //         the story of a girl crashing her ex-boyfriend's wedding. While Swift 
-    //         has never objected to anyone's wedding (in real life), the song is 
-    //         rumored to be about her friend Hayley Williams. I like how the song is 
-    //         fun and upbeat.
-    //     </p>
-    //     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5xXqyjLicvEpch72qEryFT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    // </div>
-    // </>
-    // };
-
-    // function repSongs() {
-    //     return <>
-    //         <div class="song">
-    //             <h3>Look What You Made Me Do</h3>
-    //             <p>
-    //                 "Look What You Made Me Do" is definitely one of Swift's most 
-    //                 notorious songs, but its for good reason. This is the first song 
-    //                 that Swift released after facing extreme backlash during her feud 
-    //                 with Kanye West, with a music video that poked fun at her different 
-    //                 personas over the years.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1P17dC1amhFzptugyAO7Il?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    //         </div>
-    //         <div class="song">
-    //             <h3>Call It What You Want</h3>
-    //             <p>
-    //                 While Reputation was marketed as a villian or revenge album, 
-    //                 it has its fair share of love songs. "Call It What You Want" was 
-    //                 likely written about Swift's former long-term boyfriend Joe Alwyn. 
-    //                 It discusses Alwyn staying by Swift's side while she felt she was 
-    //                 losing her career and repuation.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1GwMQaZz6Au3QLDbjbMdme?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    //         </div>
-    //         <div class="song">
-    //             <h3>Getaway Car</h3>
-    //             <p>
-    //                 On the surface, "Getaway Car" tells the story of a woman betraying her 
-    //                 partner in crime. The song is likely an allegory for betrayal in 
-    //                 one of Swift's real-life relationships. I think that the 
-    //                 song is thrilling to listen to with the story's highs and lows.
-    //             </p>
-    //             <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0VE4kBnHJUgtMf0dy6DRmW?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
-    //         </div>
-    //     </>
-    // }
-
-
-    // let canvas = this.document.querySelector("canvas");
-    // console.log(canvas);
-    // let ctx = canvas.getContext("2d");
-
-
-    // this.document.getElementById("draw_hazel").addEventListener("click", function() {
-    //     let img = document.querySelector("img#hazel_img");
-    //     ctx.drawImage(img, 0, 0, 600, 600);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "red";
-    //     ctx.fillText("Hazel", 200, 50);
-        
-    // });
-
-    // this.document.getElementById("draw_biscuit").addEventListener("click", function() {
-    //     let img = document.querySelector("img#biscuit_img");
-    //     ctx.drawImage(img, 0, 0, 800, 600);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "purple";
-    //     ctx.fillText("Biscuit Dog", 150, 50);
-    // });
-
-    // this.document.getElementById("draw_bacon").addEventListener("click", function() {
-    //     let img = document.querySelector("img#bacon_img");
-    //     ctx.drawImage(img, 0, 0, 600, 600);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "red";
-    //     ctx.fillText("Bacon", 200, 50);
-    // });
-
-    // this.document.getElementById("draw_bear").addEventListener("click", function() {
-    //     let img = document.querySelector("img#bear_img");
-    //     ctx.drawImage(img, 0, 0, 600, 800);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "red";
-    //     ctx.fillText("Bear", 200, 50);
-    // });
-
-    // this.document.getElementById("draw_millie").addEventListener("click", function() {
-    //     let img = document.querySelector("img#millie_img");
-    //     ctx.drawImage(img, 0, 0, 600, 800);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "red";
-    //     ctx.fillText("Millie", 200, 50);
-    // });
-
-    // this.document.getElementById("draw_molly").addEventListener("click", function() {
-    //     let img = document.querySelector("img#molly_img");
-    //     ctx.drawImage(img, 0, 0, 600, 600);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "purple";
-    //     ctx.fillText("Molly", 200, 50);
-    // });
-
-    // this.document.getElementById("draw_carter").addEventListener("click", function() {
-    //     let img = document.querySelector("img#carter_img");
-    //     ctx.drawImage(img, 0, 0, 600, 600);
-    //     ctx.font = "60px Arial";
-    //     ctx.fillStyle = "red";
-    //     ctx.fillText("Carter", 200, 50);
-    // });
-});
-
-
 function chooseSpeakNow() {
     console.log("Called Choose Speak Now");
-    let rep_songs = document.querySelectorAll(".song_rep");
-    for(let index = 0; index < rep_songs.length; index++) {
-        console.log(index);
-        console.log(rep_songs[index].innerHTML);
-        rep_songs[index].style.display = "none";
-    }
     let folklore_songs = document.querySelectorAll(".song_folklore");
-    for(let index = 0; index < folklore_songs.length; index++) {
-        folklore_songs[index].style.display = "none";
-    }
+    let rep_songs = document.querySelectorAll(".song_rep");
     let speak_songs = document.querySelectorAll(".song_speak");
-    for(let index = 0; index < speak_songs.length; index++) {
-        speak_songs[index].style.display = "block";
+
+    //use animation to send them out
+    for(let index = 0; index < rep_songs.length; index++) {
+        rep_songs[index].style.animation = "flyOut 0.6s";
     }
+    for(let index = 0; index < folklore_songs.length; index++) {
+        folklore_songs[index].style.animation = "flyOut 0.6s";
+    }
+    for(let index = 0; index < speak_songs.length; index++) {
+        speak_songs[index].style.animation = "flyIn 1s";
+    }
+    
+    //set the display to none after 2 s
+    setTimeout( function () {
+        console.log("setting display to none");
+        for(let index = 0; index < rep_songs.length; index++) {
+            rep_songs[index].style.display = "none";
+        }
+        for(let index = 0; index < folklore_songs.length; index++) {
+            folklore_songs[index].style.display = "none";
+        }
+        for(let index = 0; index < speak_songs.length; index++) {
+            speak_songs[index].style.display = "block";
+        }
+    }, 400)
 
      // set the header and body class
      document.querySelector("header").className = "speak";
@@ -203,20 +37,36 @@ function chooseSpeakNow() {
 
 function chooseReputation() {
     console.log("Called Choose Reputation");
-    let rep_songs = document.querySelectorAll(".song_rep");
-    for(let index = 0; index < rep_songs.length; index++) {
-        console.log(index);
-        console.log(rep_songs[index].innerHTML);
-        rep_songs[index].style.display = "block";
-    }
     let folklore_songs = document.querySelectorAll(".song_folklore");
-    for(let index = 0; index < folklore_songs.length; index++) {
-        folklore_songs[index].style.display = "none";
-    }
     let speak_songs = document.querySelectorAll(".song_speak");
-    for(let index = 0; index < speak_songs.length; index++) {
-        speak_songs[index].style.display = "none";
+    let rep_songs = document.querySelectorAll(".song_rep");
+
+    for(let index = 0; index < rep_songs.length; index++) {
+        rep_songs[index].style.animation = "flyIn 1s";
     }
+
+    //send out other songs
+    for(let index = 0; index < folklore_songs.length; index++) {
+        folklore_songs[index].style.animation = "flyOut 0.6s";
+    }
+    for(let index = 0; index < speak_songs.length; index++) {
+        speak_songs[index].style.animation = "flyOut 0.6s";
+    }
+
+    //change visibility of other songs to 0 and send new songs in
+    setTimeout(function() {
+        for(let index = 0; index < folklore_songs.length; index++) {
+            folklore_songs[index].style.display = "none";
+        }
+
+        for(let index = 0; index < speak_songs.length; index++) {
+            speak_songs[index].style.display = "none";
+        }
+
+        for(let index = 0; index < rep_songs.length; index++) {
+            rep_songs[index].style.display = "block";
+        }
+    }, 400)
 
     // set the header and body class
     document.querySelector("header").className = "reputation";
@@ -244,5 +94,28 @@ function chooseFolklore() {
     // set the header and body class
     document.querySelector("header").className = "folklore";
     document.querySelector("body").className = "folklore";
+
+}
+
+function chooseAll() {
+    console.log("Called Choose All");
+    let rep_songs = document.querySelectorAll(".song_rep");
+    for(let index = 0; index < rep_songs.length; index++) {
+        console.log(index);
+        console.log(rep_songs[index].innerHTML);
+        rep_songs[index].style.display = "block";
+    }
+    let folklore_songs = document.querySelectorAll(".song_folklore");
+    for(let index = 0; index < folklore_songs.length; index++) {
+        folklore_songs[index].style.display = "block";
+    }
+    let speak_songs = document.querySelectorAll(".song_speak");
+    for(let index = 0; index < speak_songs.length; index++) {
+        speak_songs[index].style.display = "block";
+    }
+
+     // set the header and body class
+     document.querySelector("header").className = "choose";
+     document.querySelector("body").className = "choose";
 
 }
